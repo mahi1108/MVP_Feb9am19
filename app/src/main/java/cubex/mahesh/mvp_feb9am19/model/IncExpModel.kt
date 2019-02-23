95844ade018fe53255b2d5c6d69b0091f00483b6
maheshthippala <mahesh.cubex@gmail.com>
2019-02-23 09:04:47 +0530

    Added Implementation code..

--- app/src/main/java/cubex/mahesh/mvp_feb9am19/model/IncExpModel.kt
+++ app/src/main/java/cubex/mahesh/mvp_feb9am19/model/IncExpModel.kt
@@ -1,14 +1,43 @@
 package cubex.mahesh.mvp_feb9am19.model
 
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 import cubex.mahesh.mvp_feb9am19.beans.IncExpBean
 import cubex.mahesh.mvp_feb9am19.presenter.IncExpPresenterAPI
+import cubex.mahesh.mvp_feb9am19.view.IncExpViewAPI
+import cubex.mahesh.mvp_feb9am19.view.MainActivity
 
-class IncExpModel:IncExpPresenterAPI {
-    override fun save(bean: IncExpBean) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+class IncExpModel(var api:IncExpViewAPI):IncExpPresenterAPI {
+
+    var dBase :SQLiteDatabase ? = null
+    var mActivity : MainActivity ?= null
+
+    init {
+            mActivity = api as MainActivity
+            dBase = mActivity!!.openOrCreateDatabase(
+   "and_9am_feb19", Context.MODE_PRIVATE,null)
+            dBase?.execSQL("create table if not exists incexp(_id integer primary key autoincrement,date varchar(20),money integer,_desc varchar(500),type varchar(20) )")
     }
-
+    override fun save(bean: IncExpBean) {
+        var cv = ContentValues( )
+        cv.put("date",bean.date)
+        cv.put("money",bean.money)
+        cv.put("_desc",bean.desc)
+        cv.put("type",bean.type)
+        var status = dBase?.insert("incexp",
+            null,cv)
+        if(status !== -1L)
+            api.saveResponse("Row Inserted Successfully")
+        else
+            api.saveResponse("Row Insetion Failed..")
+    }
     override fun read() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+        var c:Cursor? = dBase?.query("incexp",null,
+            null,null,
+                            null,null,null)
+        api.readResponse(c!!)
     }
 }
--- app/src/main/java/cubex/mahesh/mvp_feb9am19/view/MainActivity.kt
+++ app/src/main/java/cubex/mahesh/mvp_feb9am19/view/MainActivity.kt
@@ -3,20 +3,55 @@ package cubex.mahesh.mvp_feb9am19.view
 import android.database.Cursor
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SimpleCursorAdapter
+import android.widget.Toast
 import cubex.mahesh.mvp_feb9am19.R
+import cubex.mahesh.mvp_feb9am19.beans.IncExpBean
+import cubex.mahesh.mvp_feb9am19.model.IncExpModel
+import cubex.mahesh.mvp_feb9am19.presenter.IncExpPresenterAPI
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(),IncExpViewAPI {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var model:IncExpPresenterAPI =
+                                        IncExpModel(this@MainActivity)
+        save.setOnClickListener {
+            var bean = IncExpBean(et_date.text.toString(),
+                et_mony.text.toString().toInt(),
+                et_desc.text.toString(),
+                sp_type.selectedItem.toString())
+            model.save(bean)
+        }
+        read.setOnClickListener {
+            model.read()
+        }
     }
 
     override fun saveResponse(msg: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        Toast.makeText(this@MainActivity,
+                        msg,Toast.LENGTH_LONG).show()
     }
 
     override fun readResponse(c: Cursor) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        var cAdapter = SimpleCursorAdapter(
+            this@MainActivity,
+            R.layout.indiview,c,
+            arrayOf("date","money","_desc","type"),
+            intArrayOf(R.id.date,R.id.money,R.id.desc,R.id.type),
+            0)
+        lview.adapter = cAdapter
+        var iSum = 0
+        var eSum = 0
+        while (c.moveToNext()){
+    if(c.getString(4).equals("income",true))
+            iSum = iSum + c.getInt(2)
+    else
+            eSum =eSum + c.getInt(2)
+        }
+        tv_income.setText("Income Sum : $iSum")
+        tv_expense.setText("Expense Sum : $eSum")
     }
 }
